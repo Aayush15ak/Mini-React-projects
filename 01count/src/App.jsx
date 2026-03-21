4cc4bdb5cd6ebce4ab6ae9e05d6c12680914de7a
Aayush15ak <quake.05.005@gmail.com>
2026-03-21 21:01:52 +0530

    Count project

---
+++ 01count/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  }
+  const decrease = () =>{
+    setCount(count - 1);
+  }
+
+  return (
+    <>
+      <h1>This is my first React app</h1>
+      <h3>Counter: {count}</h3>
+
+      <button onClick={increase}>Tap to increase</button>
+      <br /><br />
+      <button onClick={decrease}>Tap to decrease</button>
+    </>
+  );
+}
+
+export default App;
