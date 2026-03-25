be5c80fe5b9a059c776b9827e28230a7c0e7a6db
Rahul Sharma <quake.05.005@gmail.com>
2026-03-25 23:11:48 +0530

    update

--- 03routerdom/src/components/Contact.jsx
+++ 03routerdom/src/components/Contact.jsx
@@ -1,5 +1,15 @@
 function Contact() {
-  return <h1>Contact Page</h1>
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center text-white text-5xl font-bold bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1500530855697-b586d89ba3ee)"
+      }}
+    >
+      Contact Page
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
