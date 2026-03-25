6cc4e0e8c3df3f76a1ac7556fa0e432e71dbcf4a
Rahul Sharma <quake.05.005@gmail.com>
2026-03-25 23:08:44 +0530

    solved errors

--- 03routerdom/src/main.jsx
+++ 03routerdom/src/main.jsx
@@ -10,18 +10,18 @@ import {
 } from "react-router-dom";
 
 import Layout from "./Layout.jsx";
-import Home from "./components/Home.jsx";
-import About from "./components/About.jsx";
-import Contact from "./components/Contact.jsx";
 import User from "./components/User.jsx";
+import Contact from "./components/contact.jsx";
+import About from "./components/about.jsx";
+import Home from "./components/home.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
 
-      <Route path="" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
+      <Route path="" element={<Home} />
+      <Route path="about" element={<About} />
+      <Route path="contact" element={<Contact/>} />
       <Route path="user" element={<User />} />
       <Route path="user/:id" element={<User />} />
       <Route loader={Gitloader} path="github" element={<Github/>} />
